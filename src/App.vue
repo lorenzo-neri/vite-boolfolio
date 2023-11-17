4b537afaa85676e96c8d857f78924d02061de64c
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-17 17:38:48 +0100

    print titles in page

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,22 @@ export default {
   data() {
     return {
       message: 'Welcome back Vite+Vue',
+      base_url: 'http://127.0.0.1:8000/',
+      portfolio_api: 'api/projects',
+      projects: [],
+
     }
   },
   mounted() {
-
+    axios
+      .get(this.base_url + this.portfolio_api)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.result;
+      })
+      .catch(err => {
+        console.error(err);
+      })
   }
 }
 </script>
@@ -17,6 +29,16 @@ export default {
 <template>
   <div id="app">
     {{ message }}
+
+    <div class="container">
+      <div class="row">
+        <div v-for="project in projects.data" class="col">
+          <div class="card text-center">
+            <h2>{{ project.title }}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
