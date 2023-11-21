863018964a09ee00ef485b5d8f32aa1767ededba
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-21 16:05:55 +0100

    add header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,21 @@
 <script>
+import AppHeader from './components/layouts/AppHeader.vue';
+import AppFooter from './components/layouts/AppFooter.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
 }
 
 
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">Boolfolio</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
-          aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="mainNav">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link active" to="/" aria-current="page">Home<span
-                  class="visually-hidden">(current)</span></router-link>
-            </li>
-
-
-            <!-- <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link>
-            </li>
-
-            <li class="nav-item">
-              <router-link class="nav-link" to="/blog">Blog</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/contacts">Contacts</router-link>
-            </li> -->
-
-
-          </ul>
-          <div class="admin">
-            <a href="http://127.0.0.1:8000/admin" class="btn btn-dark" target="__blank">
-              Admin <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                  d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
-                <path fill-rule="evenodd"
-                  d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-              </svg>
-            </a>
-          </div>
-
-        </div>
-      </div>
-    </nav>
-
-  </header>
+  <AppHeader>
+  </AppHeader>
 
   <!-- MAIN -->
   <main style="background-color: #76BED0;" class="py-4">
@@ -61,107 +24,7 @@ export default {
 
   </main>
 
-  <footer class="py-5 bg-dark text-light">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <h3>Lorem.</h3>
-          <ul class="list-unstyled">
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-          </ul>
-        </div>
-        <div class="col">
-          <h3>Lorem.</h3>
-          <ul class="list-unstyled">
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-          </ul>
-        </div>
-        <div class="col">
-          <h3>Lorem.</h3>
-          <ul class="list-unstyled">
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-          </ul>
-        </div>
-        <div class="col">
-          <h3>Lorem.</h3>
-          <ul class="list-unstyled">
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Lorem</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <!-- /.container -->
-
-    <div style="background-color: #1b1b1b;">
-
-      <div class="container">
-
-        <div class="row align-items-center mt-4 p-2">
-
-          <div class="col">
-            &copy; 2023 - Lorenzo Neri - Tutti i diritti sono riservati.
-          </div>
-
-          <div class="col d-flex align-items-center justify-content-end gap-3">
-
-            <!-- linkedin -->
-            <a href="https://www.linkedin.com/in/lorenzo-neri-a32248262/">
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-linkedin"
-                viewBox="0 0 16 16">
-                <path
-                  d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-              </svg>
-
-            </a>
-            <!-- /linkedin -->
-
-            <!-- github -->
-            <a href="https://github.com/lorenzo-neri">
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-github"
-                viewBox="0 0 16 16">
-                <path
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
-
-            </a>
-            <!-- /github -->
-
-            <!-- mail -->
-            <a href="mailto:lorenzo21neri@gmail.com">
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                class="bi bi-envelope-fill" viewBox="0 0 16 16">
-                <path
-                  d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-              </svg>
-
-            </a>
-            <!-- /mail -->
-
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </footer>
+  <AppFooter />
 </template>
 
 <style lang=scss scoped>
---
+++ src/components/layouts/AppHeader.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+
+
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Boolfolio</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+                    aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainNav">
+                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <router-link class="nav-link active" to="/" aria-current="page">Home<span
+                                    class="visually-hidden">(current)</span></router-link>
+                        </li>
+
+
+                        <!-- <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link class="nav-link" to="/blog">Blog</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contacts">Contacts</router-link>
+            </li> -->
+
+
+                    </ul>
+                    <div class="admin">
+                        <a href="http://127.0.0.1:8000/admin" class="btn btn-dark" target="__blank">
+                            Admin <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                                <path fill-rule="evenodd"
+                                    d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                            </svg>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+
+    </header>
+</template>
+
+<style lang=scss scoped>
+$bln-primary: #76BED0;
+$bln-secondary: #F55D3E;
+$bln-success: #F7CB15;
+$bln-light: #ffff;
+$bln-dark: #1b1b1b;
+
+a {
+    color: white;
+    text-decoration: none;
+    transition: .5s;
+
+    &:hover {
+        color: $bln-secondary;
+    }
+}
+</style>
